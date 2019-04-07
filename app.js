5bc44f9be078c161e83530eed8c8d71047cd049d
ecemyilmaz <ecemplusmax@gmail.com>
2019-04-07 18:11:52 +0300

    write getbmivalue function

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('hello');
+function getBmiValue() {
+  const weight = Number(document.getElementById('weight').value);
+  const height = Number(document.getElementById('height').value);
+  const result = weight / (height * height);
+  document.getElementById('result').innerHTML = `Your bmi value is : ${result}`;
+}
