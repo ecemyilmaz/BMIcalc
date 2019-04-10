ae533e1c6444d06d5e6c689c096ecad32c185b26
ecemyilmaz <ecemplusmax@gmail.com>
2019-04-10 22:58:17 +0300

    Add round-off feature

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 function getBmiValue() {
   const weight = Number(document.getElementById('weight').value);
   const height = Number(document.getElementById('height').value);
-  const result = weight / (height * height);
+  const result = Math.round(weight / (height * height));
   // document.getElementById('bmi_value').innerHTML = `Your bmi value is : ${result}`;
-  document.getElementById('result').innerHTML = result;
-  // document.getElementById('result').innerHTML = `Your bmi value is : ${result}`;
+  // document.getElementById('result').innerHTML = result;
+  document.getElementById('result').innerHTML = `Your bmi value is : ${result}`;
 }
