94016e4dc9d62167f1c24caead21e716be2081e7
ecemyilmaz <ecemplusmax@gmail.com>
2019-04-14 20:21:41 +0300

    Add more features

--- app.js
+++ app.js
@@ -9,3 +9,17 @@ function getBmiValue() {
     'result'
   ).innerHTML = `${name}, your bmi value is : ${result}`;
 }
+
+function check() {
+  const age = document.fform.age.value;
+  const bmi = document.fform.bmi.value;
+  const sex = parseInt(document.getElementById('male').checked ? 1 : 0);
+  let fat;
+
+  if (age <= 15) {
+    fat = Math.round((1.51 * bmi - 0.7 * age - 3.6 * sex + 1.4) * 100) / 100;
+  } else {
+    fat = Math.round((1.2 * bmi + 0.23 * age - 10.8 * sex - 5.4) * 100) / 100;
+  }
+  document.getElementById('fat').value = fat;
+}
