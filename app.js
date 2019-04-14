253d5074c4152796674249543a5e548c7cec028e
ecemyilmaz <ecemplusmax@gmail.com>
2019-04-14 21:55:50 +0300

    Add more features

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ function getBmiValue() {
   const height = Number(document.getElementById('height').value);
   const name = document.getElementById('name').value;
   const result = Math.round(weight / (height * height));
-  // document.getElementById('bmi_value').innerHTML = `Your bmi value is : ${result}`;
-  // document.getElementById('result').innerHTML = result;
   document.getElementById(
     'result'
   ).innerHTML = `${name}, your bmi value is : ${result}`;
@@ -13,13 +11,14 @@ function getBmiValue() {
 function check() {
   const age = document.fform.age.value;
   const bmi = document.fform.bmi.value;
-  const sex = parseInt(document.getElementById('male').checked ? 1 : 0);
+  const gender = parseInt(document.getElementById('male').checked ? 1 : 0);
   let fat;
 
   if (age <= 15) {
-    fat = Math.round((1.51 * bmi - 0.7 * age - 3.6 * sex + 1.4) * 100) / 100;
+    fat = Math.round((1.51 * bmi - 0.7 * age - 3.6 * gender + 1.4) * 100) / 100;
   } else {
-    fat = Math.round((1.2 * bmi + 0.23 * age - 10.8 * sex - 5.4) * 100) / 100;
+    fat =
+      Math.round((1.2 * bmi + 0.23 * age - 10.8 * gender - 5.4) * 100) / 100;
   }
   document.getElementById('fat').value = fat;
 }
